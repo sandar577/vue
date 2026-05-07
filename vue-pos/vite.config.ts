35a7af604ea28ac01bd256a6260c5cceb5aa4e13
Sandar Win <sander337w@prestigein.com>
2026-05-08 04:15:21 +0630

    updated config

--- vue-pos/vite.config.ts
+++ vue-pos/vite.config.ts
@@ -7,6 +7,7 @@ import tailwindCSS from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/vue/',
   plugins: [vue(), vueDevTools(), tailwindCSS()],
   resolve: {
     alias: {
